3200f42c86211ae98fa2db484722db1dfed79569
Alexey Mytcykov <alexmyt1@yandex.ru>
2019-02-25 18:18:09 +0400

    add untracked files

---
+++ app/Http/Controllers/API/TaxiServiceAPIController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\TaxiService;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TaxiServiceAPIController extends Controller
+{
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $allTaxiServices= TaxiService::all();
+        return $allTaxiServices;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TaxiService  $taxiService
+     * @return \Illuminate\Http\Response
+     */
+    public function show($slug)
+    {
+        return TaxiService::whereSlug($slug)->firstOrFail();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TaxiService  $taxiService
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TaxiService $taxiService)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TaxiService  $taxiService
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TaxiService $taxiService)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TaxiService  $taxiService
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TaxiService $taxiService)
+    {
+        //
+    }
+}
